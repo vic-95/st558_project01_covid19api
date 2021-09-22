fcd2bdeeb91ce4f212e0dc87bb0f03c9e882c2f0
Vic Seng <vic.seng95@gmail.com>
2021-09-21 20:40:48 -0400

    first commit, adding gitignore, readme and code to create readme.

---
+++ README.md
@@ -0,0 +1,6 @@
+README
+================
+Victoria Seng
+9/21/2021
+
+# This is the readme file. Contents to be updated.
---
+++ _R/readme_render.R
@@ -0,0 +1,12 @@
+## The render code to output the README.md file
+## This code assumes that the working directory is 1 level up from the /_Rmd directory
+
+rmarkdown::render("./_Rmd/README.Rmd", 
+                  output_format = "github_document",
+                  output_dir = "./", 
+                  output_options = list(
+                    toc = FALSE, 
+                    html_preview = FALSE, 
+                    keep_html = FALSE
+                  )
+)
