ffe5dc4256ce3816f9b276fbb207fc5914596675
Vic Seng <vic.seng95@gmail.com>
2021-10-05 20:59:34 -0400

    adding the vignette to the README because I can't read aparrently

--- _R/vignette_render.R
+++ _R/vignette_render.R
@@ -1,7 +1,7 @@
 ## The render code to output the vignette.md file
 ## This code assumes that the working directory is 1 level up from the /_Rmd directory
 
-rmarkdown::render("../_Rmd/vignette.Rmd", 
+rmarkdown::render("../_Rmd/README.Rmd", 
                   output_format = "github_document",
                   output_dir = "./", 
                   output_options = list(
